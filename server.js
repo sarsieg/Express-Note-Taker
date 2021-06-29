63746ef9c5e7b5427d4e71a393ac51f7f0fbdc16
Sarah Siegel <sarah.ac.siegel@gmail.com>
2021-06-29 17:44:43 -0500

    added app.use functions and join function to get the information and join it into the html file

--- server.js
+++ server.js
@@ -5,3 +5,11 @@ const path = require("path");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
+app.use(express.json());
+
+app.get("/", (req, res) => {
+    res.sendFile(path.join(_dirname, "/public/index.html"));
+});
