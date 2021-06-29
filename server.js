1ac8e7dd4d10bba6179db3bc306113822963be06
Sarah Siegel <sarah.ac.siegel@gmail.com>
2021-06-29 18:22:29 -0500

    fixed some stuff

--- server.js
+++ server.js
@@ -2,8 +2,7 @@
 const fs = require("fs");
 const express = require("express");
 const path = require("path");
-const { debugPort } = require("process");
-const { json } = require("body-parser");
+
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -16,11 +15,11 @@ app.get("/", (req, res) => {
     res.sendFile(path.join(_dirname, "/public/index.html"));
 });
 
-app.get("/", (req, res) => {
+app.get("/notes", (req, res) => {
     res.sendFile(path.join(_dirname, "/public/notes.html"));
 });
 
-app.get("/notes", (req, res) => {
+app.get("/api/notes", (req, res) => {
     res.readFile(path.join(_dirname, "/db/db.json"), "utf8", (err, data) => {
         if (err) throw err;
         res.json(JSON.parse(data));
@@ -48,37 +47,37 @@ app.post("/api/notes", (req, res) => {
         fs.writeFile(path.join(_dirname, "/db/db.json"), JSON.stringify(newDB, null, 2), (err) => {
             if (err) throw err;
             res.json(req.body);
-        });
-    });
-});
+        })
+    })
+})
 
 app.delete("/api/notes/:id"), (req, res) => {
-const id = parseInt(req.params.id);
-fs.readFile(path.join(_dirname, "/db/db.json"), "utf8", (err, data) => {
-    if (err) throw err;
-    const db = JSON.parse(data);
-    const newDB = [];
-
-    for (let i = 0; i < db.length; i++) {
-        if (i !== id) {
-            const newNote = {
-                title: db[i].title,
-                text: db[i].text,
-                id: i
-            };
-
-            newDB.push(newNote);
-        }
-    }
-
-    fs.writeFile(path.join(_dirname, "/db/db.json"), JSON.stringify(newDB, null, 2), (err) => {
+    const id = parseInt(req.params.id);
+    fs.readFile(path.join(_dirname, "/db/db.json"), "utf8", (err, data) => {
         if (err) throw err;
-        res.json(req.body);
-    });
-});
-});
+        const db = JSON.parse(data);
+        const newDB = [];
+
+        for (let i = 0; i < db.length; i++) {
+            if (i !== id) {
+                const newNote = {
+                    title: db[i].title,
+                    text: db[i].text,
+                    id: i
+                };
+
+                newDB.push(newNote);
+            }
+        }
+
+        fs.writeFile(path.join(_dirname, "/db/db.json"), JSON.stringify(newDB, null, 2), (err) => {
+            if (err) throw err;
+            res.json(req.body);
+        })
+    })
+}
 
 app.listen(PORT, () => {
     console.log(`App listening on PORT &{PORT}.`);
 
-})
+});
