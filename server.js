aba6059326dfb76b1b2c037597f26d0f81b5f785
Sarah Siegel <sarah.ac.siegel@gmail.com>
2021-06-29 17:54:38 -0500

    added functions to recieve a new note and save on the request body and add it to db.json file, and added new properties based on the length and assign it to each json object

--- server.js
+++ server.js
@@ -2,6 +2,8 @@
 const fs = require("fs");
 const express = require("express");
 const path = require("path");
+const { debugPort } = require("process");
+const { json } = require("body-parser");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -22,5 +24,30 @@ app.get("/notes", (req, res) => {
     res.readFile(path.join(_dirname, "/db/db.json"), "utf8", (err, data) => {
         if (err) throw err;
         res.json(JSON.parse(data));
+    });
+});
+
+app.post("/api/notes", (req, res) => {
+    fs.readFile(path.join(_dirname, "/db/db.json"), "utf8", (err, data) => {
+        if (err) throw err;
+        const db = JSON.parse(data);
+        const newDB = [];
+
+        db.push(req.body);
+
+        for (let i = 0; i < db.length; i++) {
+            const newNote = {
+                title: db[i].title,
+                text: db[i].text,
+                id: i
+            };
+
+            newDB.push(newNote);
+        }
+
+        fs.writeFile(path.join(_dirname, "/db/db.json"), JSON.stringify(newDB, null, 2), (err) => {
+            if (err) throw err;
+            res.json(req.body);
+        });
     });
 });
