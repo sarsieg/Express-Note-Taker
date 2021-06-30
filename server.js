c26003afa1c2da1e575f60cb53d08e0ac62a252b
Sarah Siegel <sarah.ac.siegel@gmail.com>
2021-06-29 21:20:48 -0500

    changed link to bring you to index.html page

--- server.js
+++ server.js
@@ -22,8 +22,9 @@ app.get("/api/notes", (req, res) => {
     res.sendFile(path.join(__dirname, "/db/db.json"));
 });
 
+// brings you to index.html
 app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "/db/db.json"));
+    res.sendFile(path.join(__dirname, "/public/index.html"));
 });
 
 app.post("/api/notes", (req, res) => {
